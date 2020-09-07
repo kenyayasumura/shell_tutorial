13e1498ee76414c2de81de3815445384e0915777
kenyayasumura <kennyyasumura@yahoo.co.jp>
2020-09-07 12:54:04 +0900

    UpdateGrep

--- grep.sh
+++ grep.sh
@@ -6,6 +6,7 @@
 # また、ファイルを指定しなかった場合は、標準入力から読み込む。
 
 # 参考：https://www.youtube.com/watch?v=VGgTmxXp7xQ
+# This helps me a lot to get this command. Thank you, Corey :)
 
 for fileName in ./outputs/grep1.txt ./outputs/grep2.txt ./outputs/grep3.txt ./outputs/phone.txt
 do
@@ -17,7 +18,7 @@ do
             echo "$fileName：適当に名前を入力してください"
         fi
         read -r insertedTextText
-        echo "$insertedTextText" >> $fileName
+        echo "$insertedTextText" > $fileName
     fi
 
     echo 'ando takaki sakamoto yasumura ANDO TAKAKI SAKAMOTO YASUMURA' >> outputs/grep1.txt
@@ -29,62 +30,62 @@ done
 #ヒットしたものすべて表示
 printf "\n grep 'yasumura' outputs/grep1.txt \n"
 grep 'yasumura' outputs/grep1.txt
-printf "\n\n---------------------------\n\n"
+printf "\n---------------------------\n\n"
 
 #完全一致(-w)だけ表示
 printf "\n grep -w 'yasumura' outputs/grep1.txt \n"
 grep -w 'yasumura' outputs/grep1.txt
-printf "\n\n---------------------------\n\n"
+printf "\n---------------------------\n\n"
 
 #完全一致でかつ、大文字小文字を区別せず(-i)表示
 printf "\n grep -wi 'yasumura' outputs/grep1.txt \n"
 grep -wi 'YaSuMuRa' outputs/grep1.txt
-printf "\n\n---------------------------\n\n"
+printf "\n---------------------------\n\n"
 
 #完全一致でかつ、大文字小文字を区別せず、行数(-n)も表示
 printf "\n grep -win 'yasumura' outputs/grep1.txt \n"
 grep -win 'YASUMURA' outputs/grep1.txt
-printf "\n\n---------------------------\n\n"
+printf "\n---------------------------\n\n"
 
 #検索結果から５行前(-B num)まで表示
 printf "\n grep -win -B 5 'yasumura' outputs/grep1.txt \n"
 grep -win -B 5 'yasumura' outputs/grep1.txt
-printf "\n\n---------------------------\n\n"
+printf "\n---------------------------\n\n"
 
 #検索結果から５行後(-A num)まで表示
 printf "\n grep -win -A 5 'yasumura' outputs/grep1.txt \n"
 grep -win -A 5 'yasumura' outputs/grep1.txt
-printf "\n\n---------------------------\n\n"
+printf "\n---------------------------\n\n"
 
 #検索結果から３行前後(-C num)まで表示
 printf "\n grep -win -C 3 'yasumura' outputs/grep1.txt \n"
 grep -win -C 3 'yasumura' outputs/grep1.txt
-printf "\n\n---------------------------\n\n"
+printf "\n---------------------------\n\n"
 
 #現在のディレクトリ配下すべて検索
 printf "\n grep -wi 'yasumura' ./* \n"
 grep -win 'yasumura' ./*
-printf "\n\n---------------------------\n\n"
+printf "\n---------------------------\n\n"
 
 #サブディレクトリも含めすべて検索(-r)
 printf "\n grep -winr 'yasumura' . \n"
 grep -winr 'yasumura' .
-printf "\n\n---------------------------\n\n"
+printf "\n---------------------------\n\n"
 
 #検索対象が見つかったファイル名のみ表示(-l)
 printf "\n grep -wirl 'yasumura' . \n"
 grep -wirl 'yasumura' .
-printf "\n\n---------------------------\n\n"
+printf "\n---------------------------\n\n"
 
 #検索結果の数も表示(-c)
 printf "\n grep -wirc 'yasumura' . \n"
 grep -wirc 'yasumura' .
-printf "\n\n---------------------------\n\n"
+printf "\n---------------------------\n\n"
 
 #複数のコマンドをつなげる(|)
 printf "\n history | grep 'ls' | grep 'a' \n"
 history | grep 'ls' | grep 'a'
-printf "\n\n---------------------------\n\n"
+printf "\n---------------------------\n\n"
 
 #正規表現を使う (.)すべての文字１文字、（d)すべての数字、{num}文字数
 #grep '...-...-...' outputs/phone.txt
