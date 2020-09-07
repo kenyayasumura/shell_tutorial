b444f4a7954a599290961397aaadb2d633fc2e43
kenyayasumura <kennyyasumura@yahoo.co.jp>
2020-09-07 11:55:00 +0900

    UpdateBase

--- base.sh
+++ base.sh
@@ -1,13 +1,9 @@
 #!/bin/bash
 
 echo ファイル名を入力してください。
-read file_name
-
-touch ${file_name}.sh
-
-echo "#!/bin/bash" > ${file_name}.sh
-
-chmod u+x ${file_name}.sh
+read -r file_name
+touch "${file_name}".sh
+echo "#!/bin/bash" > "${file_name}".sh
+chmod u+x "${file_name}".sh
 
 echo 完了
-
