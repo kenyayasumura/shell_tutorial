8b9f46a83a6f64cb83e4f2dac98f55c353362728
kenyayasumura <kennyyasumura@yahoo.co.jp>
2020-09-10 20:36:42 +0900

    UpdateTR

--- tr.sh
+++ tr.sh
@@ -3,7 +3,7 @@
 # trとは：
 # 指定した文字列を削除したり、別の文字列に置き換えたりできる。
 
-if [ ! -e ./outputs/tr1.txt ]; then
+if [ ! -e <(find outputs/tr*) ]; then
     touch ./outputs/tr1.txt
     echo 'Ask what your country can do for you; ask what you can do for your country' > outputs/tr1.txt
 fi
@@ -13,3 +13,6 @@ cat outputs/tr1.txt | tr ' ' '\n' | tr -d '.;' | tr 'A-Z' 'a-z' | sort
 
 # 出力する値を反転（-c）
 cat outputs/tr1.txt | tr -dc 'what'
+
+# 特定の文字を条件に一つにまとめる(-s)
+cat outputs/tr1.txt | tr -s '\n'
